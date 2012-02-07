d0f9271c9ca4cb66b5445ad4b492ed3ccf1ef174
Philip Pemberton <philpem@philpem.me.uk>
2012-02-07 13:27:02 +0000

    add sound format and colour space decoding

--- demux.cpp
+++ demux.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <cstdint>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,7 +19,7 @@ using namespace std;
 ///////////////////////////////////////////////////////////////////////////////
 
 // trim spaces from a string
-void TrimSpaces( string& str)
+void TrimSpaces(string &str)
 {
 	// Trim Both leading and trailing spaces
 	size_t startpos = str.find_first_not_of(" \t"); // Find the first character position after excluding leading blank spaces
@@ -43,7 +44,7 @@ class ENotAReplayMovie : public exception {
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-const char * videoFormats[] = {
+const char * S_VIDEOFORMATS[] = {
 	"<< no video >>",
 	"Moving Lines",
 	"16bpp uncompressed",
@@ -72,6 +73,93 @@ const char * videoFormats[] = {
 	"6bpp YYYYd4UVd4 chroma H/V subsampled by 2"
 };
 
+// There are five different Replay sound formats under SoundFormat 1, plus
+// the Custom sound format (SoundFormat 2).
+// All SF1 formats support both Mono and Stereo.
+typedef enum {
+	SOUND_4BIT_ADPCM,				// ADPCM, 4-bit, either mono or stereo
+	SOUND_8BIT_LINEAR_SIGNED,		// 8-bit Linear, Signed
+	SOUND_8BIT_LINEAR_UNSIGNED,		// 8-bit Linear, Unsigned
+	SOUND_8BIT_ULAW,				// 8-bit Exponential mu-Law
+	SOUND_16BIT_LINEAR_SIGNED,		// 16-bit Linear, Signed
+	SOUND_CUSTOM,					// Custom sound format (use the SoundFormat string)
+	SOUND_NONE,						// No audio track
+	SOUND_UNKNOWN,					// No idea what type of audio track, or something invalid
+} E_SOUND_FORMAT;
+
+// Replay supports YUV and RGB colour
+typedef enum {
+	COLOURSPACE_RGB,
+	COLOURSPACE_YUV,
+	COLOURSPACE_UNKNOWN
+} E_COLOUR_SPACE;
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Decode a sound format string (the bits-per-sample string)
+ *
+ * @param formatString	Sound format / bits per sample string
+ * @returns an E_SOUND_FORMAT tag representing the audio format.
+ */
+E_SOUND_FORMAT decodeSoundFormat(string formatString)
+{
+	string str;
+	istringstream ist;
+	int bits;
+
+	// convert format string to upper case
+	std::transform(formatString.begin(), formatString.end(), str.begin(), ::toupper);
+
+	// get bits per sample
+	ist.str(formatString);
+	ist >> bits;
+
+	// 0 bits means no sound!
+	if (bits == 0) return SOUND_NONE;
+
+	// check for linear, ADPCM or exponential
+	if (str.find("LIN") != string::npos) {
+		// Linear -- check for signedness
+		if (str.find("UNSIGN") != string::npos) {
+			// Linear unsigned
+			switch (bits) {
+				case 8:  return SOUND_8BIT_LINEAR_UNSIGNED;
+				case 16:    // 16 Linear Unsigned is not valid per the Acorn Replay spec!
+				default: return SOUND_UNKNOWN;
+			}
+		} else {
+			// Linear signed
+			switch (bits) {
+				case 8:  return SOUND_8BIT_LINEAR_SIGNED;
+				case 16: return SOUND_16BIT_LINEAR_SIGNED;
+				default: return SOUND_UNKNOWN;
+			}
+		}
+	} else if ((bits == 4) || (str.find("ADPCM") != string::npos)) {
+		// 4-bit Linear ADPCM
+		return SOUND_4BIT_ADPCM;
+	} else {
+		// must be 8-bit Exponential mu-Law / u-Law then...
+		return SOUND_8BIT_ULAW;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+class ReplayCatalogue {
+
+	vector<size_t>		chunkOffsets, videoSizes, soundSizes;
+
+	// load catalogue
+	ReplayCatalogue(istream &is, size_t numChunks)
+	{
+		// TODO implement this
+	}
+};
+
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -84,6 +172,7 @@ class ReplayHeader {
 		uint32_t		xSize, ySize;
 		float			xAspect, yAspect;
 		uint32_t		bpp;
+		E_COLOUR_SPACE	colourSpace;
 		float			fps;
 		uint32_t		iSoundFormat;
 		string			sSoundFormat;
@@ -123,12 +212,12 @@ class ReplayHeader {
 			// video compression format
 			std::getline(stream, sVideoFormat); TrimSpaces(sVideoFormat); ist.str(sVideoFormat);
 			ist >> iVideoFormat;
-			// TODO: compression format parameter list
+			// TODO: decode the compression format parameter list
 
 			// X size in pixels
 			std::getline(stream, st); TrimSpaces(st); ist.str(st);
 			ist >> xSize;
-			// TODO: optional aspect ratio
+			// TODO: decode optional aspect ratio
 
 			// Y size in pixels
 			std::getline(stream, st); TrimSpaces(st); ist.str(st);
@@ -137,6 +226,15 @@ class ReplayHeader {
 			// Pixel depth in bits
 			std::getline(stream, st); TrimSpaces(st); ist.str(st);
 			ist >> bpp;
+			// This line also encodes the colour space type (YUV or RGB)
+			std::transform(st.begin(), st.end(), st.begin(), ::toupper);
+			if (st.find("YUV") != string::npos) {
+				colourSpace = COLOURSPACE_YUV;
+			} else if (st.find("RGB") != string::npos) {
+				colourSpace = COLOURSPACE_RGB;
+			} else {
+				colourSpace = COLOURSPACE_UNKNOWN;
+			}
 
 			// Number of frames per second
 			std::getline(stream, st); TrimSpaces(st); ist.str(st);
@@ -206,8 +304,8 @@ void ReplayHeader::dump(void)
 	cout << "Copyright:    [" << copyright << "]" << endl;
 	cout << "Author:       [" << author << "]" << endl;
 	cout << "Video format: [" << sVideoFormat << "], fmt id " << iVideoFormat << endl << "\t";
-	if (iVideoFormat < NELEMS(videoFormats))
-		cout << videoFormats[iVideoFormat] << endl;
+	if (iVideoFormat < NELEMS(S_VIDEOFORMATS))
+		cout << S_VIDEOFORMATS[iVideoFormat] << endl;
 	else if ((iVideoFormat >= 100) && (iVideoFormat < 200))
 		cout << "EIDOS " << iVideoFormat << endl;
 	else if ((iVideoFormat >= 200) && (iVideoFormat < 300))
