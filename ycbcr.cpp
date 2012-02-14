bdb6850cc7dc2dfb4ccca736f18ee88d3fa70592
Philip Pemberton <philpem@philpem.me.uk>
2012-02-14 09:59:17 +0000

    optimise YCbCr (YUV) to RGB translator

--- ycbcr.cpp
+++ ycbcr.cpp
@@ -15,7 +15,7 @@ typedef struct {
 } YUV_TRIPLET;
 
 typedef struct {
-	int r, g, b;
+	unsigned char r, g, b;
 } RGB_TRIPLET;
 
 RGB_TRIPLET YUV2RGB(int y, int u, int v)
@@ -23,18 +23,20 @@ RGB_TRIPLET YUV2RGB(int y, int u, int v)
 	RGB_TRIPLET t;
 	int r,g,b;
 
+	// YUV to RGB conversion
 	r = (1.164*(y/*-16*/))                     + (2.018*(u-128));
 	g = (1.164*(y/*-16*/)) - (0.813 * (v-128)) - (0.391*(u-128));
 	b = (1.164*(y/*-16*/)) + (1.596 * (v-128));
 
 	// clip values
-	if (r > 255) t.r = 255;
-	if (g > 255) t.g = 255;
-	if (b > 255) t.b = 255;
-	if (r < 0) t.r = 0;
-	if (g < 0) t.g = 0;
-	if (b < 0) t.b = 0;
+	if (r > 255) r = 255;
+	if (g > 255) g = 255;
+	if (b > 255) b = 255;
+	if (r < 0) r = 0;
+	if (g < 0) g = 0;
+	if (b < 0) b = 0;
 
+	// copy into result block
 	t.r = r;
 	t.g = g;
 	t.b = b;
@@ -50,7 +52,7 @@ RGB_TRIPLET YUV2RGB(YUV_TRIPLET x)
 int main(void)
 {
 	RGB_TRIPLET t = YUV2RGB(128, 128, 0);
-
+/*
 	CImg<int> img(256,256,1,3);
 
 	int y, u, v;
@@ -64,6 +66,29 @@ int main(void)
 		}
 	}
 	img.display();
+*/
+	RGB_TRIPLET yuvtable[0x20][0x20][0x20];
+	int y=0,u=0,v=0;
+	for (y=0; y<0x20; y++) {
+		for (u=0; u<0x20; u++) {
+			for (v=0; v<0x20; v++) {
+				yuvtable[y][u][v] = YUV2RGB(y*8,u*8,v*8);
+			}
+		}
+	}
+
+	CImg<int> k(256,256,1,3);
+	for (y=0; y<256; y+=127) {
+	//	y=128;
+		for (v=0; v<256; v++) {
+			for (u=0; u<256; u++) {
+				k(v, 255-u, 0, 0) = yuvtable[y/8][u/8][v/8].r;
+				k(v, 255-u, 0, 1) = yuvtable[y/8][u/8][v/8].g;
+				k(v, 255-u, 0, 2) = yuvtable[y/8][u/8][v/8].b;
+			}
+		}
+		k.display();
+	}
 
 	return 0;
 }
